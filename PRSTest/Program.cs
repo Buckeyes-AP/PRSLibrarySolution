8ddebc8f9e1790c45668fbc7c439a566bd085138
Alex Poirier <alexmatthewpoirier@gmail.com>
2022-02-14 16:14:19 -0500

    Added a Controller

--- PRSTest/Program.cs
+++ PRSTest/Program.cs
@@ -1,9 +1,52 @@
-﻿using System;
+﻿using PRSLibrary.Controllers;
+using PRSLibrary.Models;
+using System;
 
 namespace PRSTest {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+
+
+            var context = new PrsDbContext();
+
+            var userCtrl = new UsersController(context);
+
+           
+
+            var newUser = new User() {
+                Id = 0, Username = "yy", Password = "xx",
+                Firstname = "User", Lastname = "YY",
+                Phone = "211", Email = "xxx@user.com",
+                IsReviewer = false, IsAdmin = false
+            };
+
+            userCtrl.Create(newUser);
+
+            var user3 = userCtrl.GetbyPk(3);
+
+            if (user3 is null) {
+                Console.WriteLine("User not found!");
+            } else { 
+                Console.WriteLine($"User3: {user3.Firstname} {user3.Lastname}"); }
+
+            var user33 = userCtrl.GetbyPk(33);
+
+            if (user33 is null) {
+                Console.WriteLine("User not found!");
+            } else {
+                Console.WriteLine($"User33: {user33.Firstname} {user33.Lastname}");
+            }
+
+
+
+            var users = userCtrl.GetAll();
+
+            foreach(var user in users) {
+                Console.WriteLine($"{user.Firstname} {user.Lastname}");
+            }
+
+
         }
     }
 }
---
+++ PRSTest/UserController.cs
@@ -0,0 +1,11 @@
+﻿using PRSLibrary.Models;
+
+namespace PRSTest {
+    internal class UserController {
+        private PrsDbContext context;
+
+        public UserController(PrsDbContext context) {
+            this.context = context;
+        }
+    }
+}
