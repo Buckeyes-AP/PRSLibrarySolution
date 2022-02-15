c2366c7246baf3eea62d42b8f076dd7cf343b31c
Alex Poirier <alexmatthewpoirier@gmail.com>
2022-02-15 08:53:01 -0500

    Updated program for Vendors

---
+++ PRSLibrary/Controllers/VendorsController.cs
@@ -0,0 +1,50 @@
+﻿using PRSLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRSLibrary.Controllers {
+    public class VendorsController {
+
+        private readonly PrsDbContext _context;
+
+        public VendorsController(PrsDbContext context) {
+            this._context = context;
+
+        }
+
+        public IEnumerable<Vendor> GetAll() {
+            return _context.Vendors.ToList();
+        }
+
+        public Vendor GetbyPk(int id) {
+            return _context.Vendors.Find(id);
+        }
+
+        public Vendor Create(Vendor vendor) {
+            if(vendor is null) {
+                throw new ArgumentNullException("vendor");
+            }
+            if (vendor.Id != 0) {
+                throw new ArgumentException("Vendor.Id must be zero!");
+            }
+            _context.Vendors.Add(vendor);
+            _context.SaveChanges();
+            return vendor;
+
+        }
+
+        public void Remove(int id) {
+            var vendor = _context.Vendors.Find(id);
+            if(vendor is null) {
+                throw new Exception("Vendor not found!");
+            }
+            _context.Vendors.Remove(vendor);
+            _context.SaveChanges();
+        }
+        
+    }
+
+}
--- PRSTest/Program.cs
+++ PRSTest/Program.cs
@@ -6,48 +6,76 @@ namespace PRSTest {
     class Program {
         static void Main(string[] args) {
 
-
-
             var context = new PrsDbContext();
+            var vendorCtrl = new VendorsController(context);
 
-            var userCtrl = new UsersController(context);
+            var newVendor = new Vendor() {
+                Id = 0, Code = "cc", Name = "ff", Address = "xx",
+                City = "Cincinnati", State = "Oh", Zip = "45200",
+                Phone = "513-111-2222", Email = "cincy@cbell.com"
 
-           
-
-            var newUser = new User() {
-                Id = 0, Username = "hh", Password = "xx",
-                Firstname = "User", Lastname = "HH",
-                Phone = "211", Email = "xxx@user.com",
-                IsReviewer = false, IsAdmin = false
             };
 
-            userCtrl.Create(newUser);
+            vendorCtrl.Create(newVendor);
 
-            var user3 = userCtrl.GetbyPk(3);
-
-            if (user3 is null) {
-                Console.WriteLine("User not found!");
-            } else { 
-                Console.WriteLine($"User3: {user3.Firstname} {user3.Lastname}"); }
-
-            user3.Lastname = "User3";
-            userCtrl.Change(user3);
-
-            var user33 = userCtrl.GetbyPk(33);
-
-            if (user33 is null) {
+            var vendor3 = vendorCtrl.GetbyPk(3);
+            if (vendor3 is null) {
                 Console.WriteLine("User not found!");
             } else {
-                Console.WriteLine($"User33: {user33.Firstname} {user33.Lastname}");
+                Console.WriteLine($"Vendor3: {vendor3.Name} {vendor3.Address}");
             }
 
-            userCtrl.Remove(5);
+            var vendors = vendorCtrl.GetAll();
 
-            var users = userCtrl.GetAll();
-
-            foreach(var user in users) {
-                Console.WriteLine($"{user.Id}{user.Firstname} {user.Lastname}");
+            foreach(var vendor in vendors) {
+                Console.WriteLine($"{vendor.Id} {vendor.Code} {vendor.Name}");
             }
+
+
+
+
+
+
+            //var context = new PrsDbContext();
+
+            //var userCtrl = new UsersController(context);
+
+
+
+            //var newUser = new User() {
+            //    Id = 0, Username = "hh", Password = "xx",
+            //    Firstname = "User", Lastname = "HH",
+            //    Phone = "211", Email = "xxx@user.com",
+            //    IsReviewer = false, IsAdmin = false
+            //};
+
+            //userCtrl.Create(newUser);
+
+            //var user3 = userCtrl.GetbyPk(3);
+
+            //if (user3 is null) {
+            //    Console.WriteLine("User not found!");
+            //} else { 
+            //    Console.WriteLine($"User3: {user3.Firstname} {user3.Lastname}"); }
+
+            //user3.Lastname = "User3";
+            //userCtrl.Change(user3);
+
+            //var user33 = userCtrl.GetbyPk(33);
+
+            //if (user33 is null) {
+            //    Console.WriteLine("User not found!");
+            //} else {
+            //    Console.WriteLine($"User33: {user33.Firstname} {user33.Lastname}");
+            //}
+
+            //userCtrl.Remove(5);
+
+            //var users = userCtrl.GetAll();
+
+            //foreach(var user in users) {
+            //    Console.WriteLine($"{user.Id}{user.Firstname} {user.Lastname}");
+            //}
 
 
         }
